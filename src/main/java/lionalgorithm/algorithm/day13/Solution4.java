b02d4b12c1337f67b17e217c914a450a24a51af7
KimGunWoo9595 <medneter@naver.com>
2022-10-25 16:27:11 +0900

    set 중복제거(폰켓몬)

---
+++ src/main/java/lionalgorithm/algorithm/day13/Solution4.java
@@ -0,0 +1,25 @@
+package lionalgorithm.algorithm.day13;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution4 {
+    //https://school.programmers.co.kr/learn/courses/30/lessons/1845
+    public int solution(int[] nums) {
+        int answer = 0;
+        int max = nums.length / 2;
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num); // 중복제거
+        }
+        int size = set.size();
+        if (max < size) {
+            answer = max;
+        }else{
+            answer = size;
+        }
+
+        return answer;
+    }
+}
