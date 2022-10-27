1d485892d8e83809a0fc9426e3d2f5b3d7386986
KimGunWoo9595 <medneter@naver.com>
2022-10-27 09:35:15 +0900

    stack

--- src/main/java/lionalgorithm/algorithm/day14/ObjectArray.java
+++ src/main/java/lionalgorithm/algorithm/day14/ObjectArray.java
@@ -1,4 +1,15 @@
 package lionalgorithm.algorithm.day14;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ObjectArray {
+
+    public static void main(String[] args) {
+
+        //1.기존 - > value밖에 담을 수 없음 // 1,2
+        int[] table1;
+
+        List<HashTable.Node> table = new ArrayList<>();
+    }
 }
--- src/main/java/lionalgorithm/algorithm/day14/Solution.java
+++ src/main/java/lionalgorithm/algorithm/day14/Solution.java
@@ -1,4 +1,45 @@
 package lionalgorithm.algorithm.day14;
 
+import java.util.Stack;
+import java.util.Stack;
+
 public class Solution {
+
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i <prices.length ; i++) {
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                answer[stack.peek()] = i - stack.peek(); //stack.peek()에서는 전에 들어갔던 배열의 index번호가 있을것!
+                stack.pop();
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            answer[stack.peek()] = prices.length - 1 - stack.peek();
+            stack.pop();
+        }
+        return answer;
+    }
+
+    public int[] solutions(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> st = new Stack<>();
+        //1 2 3 2 3
+        for (int i = 0; i < prices.length; i++) {
+            if (!st.isEmpty() && prices[st.peek()] < prices[i]) {
+                //가격이 떨어진 것
+                answer[st.peek()] = i - st.peek();
+                st.pop();
+            }
+            st.push(i);
+        }
+
+        while (!st.isEmpty()) {
+            answer[st.peek()] = prices.length - 1 - st.pop();
+        }
+        return answer;
+    }
+
 }
