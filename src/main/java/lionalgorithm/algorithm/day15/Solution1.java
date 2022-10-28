7d2aea821d8a7a825bd374432c60a650db0c872c
KimGunWoo9595 <medneter@naver.com>
2022-10-28 10:13:12 +0900

    전화번호 map 이용

--- src/main/java/lionalgorithm/algorithm/day15/Solution1.java
+++ src/main/java/lionalgorithm/algorithm/day15/Solution1.java
@@ -6,25 +6,24 @@ import java.util.Map;
 public class Solution1 {
 
     //https://school.programmers.co.kr/learn/courses/13577/lessons/96091
+
     public boolean solution(String[] phone_book) {
         boolean answer = true;
 
         Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < phone_book.length; i++) {
-            map.put(phone_book[i], i); // 해싱처리완료!
+            map.put(phone_book[i], i); // 해싱처리
         }
 
 
         for (int i = 0; i < phone_book.length; i++) {
-            for (int j = 0; j < phone_book[i].length(); j++) {
+            for (int j = 1; j < phone_book[i].length(); j++) {
                 //반복문안의 반복문에서도 i를 사용 할 수 있다.
-                if (map.containsKey(phone_book[i].substring(j))) {
-
+                if (map.containsKey(phone_book[i].substring(0,j))) {// 해당전화번호를 하나하나 map의 key값이랑 비교하기
+                    answer = false;
                 }
-
             }
         }
-
 
         return answer;
     }
