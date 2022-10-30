0f59bb204cffb84d92e38634e9d48cf09acf0412
KimGunWoo9595 <medneter@naver.com>
2022-10-30 14:45:21 +0900

    가로세로 면적 알고리즘

--- src/main/java/lionalgorithm/algorithm/day14/Solution2.java
+++ src/main/java/lionalgorithm/algorithm/day14/Solution2.java
@@ -23,4 +23,25 @@ public class Solution2 {
         }
         return answer;
     }
+
+    public int[] solutions(int brown, int yellow) {
+        int[] answer = new int[2];
+        int sum = brown + yellow;
+        for (int i = 3; i < sum; i++) {
+            int garo = i;
+            int  sero = sum / garo;
+            if(sero > garo){continue;}
+            if ((garo - 2) * (sero - 2) == yellow) {
+                answer[0] = garo;
+                answer[1] = sero;
+                return answer;
+            }
+        }
+
+        return answer;
+    }
+
+
+
+
 }
