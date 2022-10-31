cf5f3c0067731d748deac522b1792b26c4bcb06f
KimGunWoo9595 <medneter@naver.com>
2022-11-01 08:49:51 +0900

    10진수를 n진수로

--- src/main/java/lionalgorithm/algorithm/day16/Solution2.java
+++ src/main/java/lionalgorithm/algorithm/day16/Solution2.java
@@ -1,4 +1,17 @@
 package lionalgorithm.algorithm.day16;
 
 public class Solution2 {
+
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        //a[0]*b[0] + a[1]*b[1] + ... + a[n-1]*b[n-1] -> 내적
+
+        for (int i = 0; i < a.length; i++) {
+          answer += a[i] * b[i];
+        }
+        return answer;
+    }
+
+
+
 }
--- src/main/java/lionalgorithm/algorithm/day16/Solution3.java
+++ src/main/java/lionalgorithm/algorithm/day16/Solution3.java
@@ -1,4 +1,26 @@
 package lionalgorithm.algorithm.day16;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution3 {
+
+    public int solution(int n) {
+        String answer = "";
+        while (n != 0) {
+            answer += n % 3;
+            n /= 3;
+        }
+        return Integer.parseInt(answer, 3);
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+
+        Solution3 s = new Solution3();
+        s.solution(45);
+    }
 }
