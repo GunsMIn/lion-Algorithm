28e4fbe8393055385eedd68cfbb2d396acae7c56
KimGunWoo9595 <medneter@naver.com>
2022-11-01 10:26:34 +0900

    탬플릿 콜백 완성(소수찾기)

--- src/main/java/lionalgorithm/algorithm/day17/templateCallback/Pick.java
+++ src/main/java/lionalgorithm/algorithm/day17/templateCallback/Pick.java
@@ -11,11 +11,21 @@ public class Pick {
         return true;
     }
 
-
-
     public static void main(String[] args) {
 
-   
+        Version1 v1 = new Version1();
+        Version2 v2 = new Version2();
+        Version3 v3 = new Version3();
+
+
+        Pick p = new Pick();
+        boolean prime1 = p.isPrime(11, v1);
+        boolean prime2 = p.isPrime(11, v2);
+        boolean prime3 = p.isPrime(11, v3);
+        System.out.println("prime1 = " + prime1);
+        System.out.println("prime2 = " + prime2);
+        System.out.println("prime3 = " + prime3);
+
 
     }
 
