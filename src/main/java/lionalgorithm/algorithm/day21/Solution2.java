eaab550b4dedb4580a4dc01a0ab9cc7106c369dc
KimGunWoo9595 <medneter@naver.com>
2022-11-07 09:27:24 +0900

    negativeArray 예외 발생

---
+++ src/main/java/lionalgorithm/algorithm/day21/Solution2.java
@@ -0,0 +1,28 @@
+package lionalgorithm.algorithm.day21;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class Solution2 {
+    //나누어 떨어지는 숫자 배열
+    //https://school.programmers.co.kr/learn/courses/30/lessons/12910
+
+    public int[] solution(int[] arr, int divisor) {
+        int[] answer = {};
+        ArrayList<Integer> list = new ArrayList<>();
+
+        //arr의 숫자배열에서 divisor 값으로 나누어 떨어지는 숫자를 반환
+        //우선 반복문을 돌려야한다.
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) {
+                list.add(arr[i]);
+            }
+        }
+        if (list.isEmpty()) {
+            return new int[-1];
+        }
+        list.stream().sorted(Comparator.naturalOrder());
+        return list.stream().mapToInt(Integer::intValue).toArray();
+
+    }
+}
