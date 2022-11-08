c583d54ba5b16655185eca78a2f11ffdf4b3d720
KimGunWoo9595 <medneter@naver.com>
2022-11-08 10:19:49 +0900

    이진 탐색 구현 시작

---
+++ src/main/java/lionalgorithm/algorithm/day22/Solution2.java
@@ -0,0 +1,21 @@
+package lionalgorithm.algorithm.day22;
+
+public class Solution2 {
+
+    public  boolean binarySearch(int []arr, int target) {
+        int start = 0;
+        int end = arr.length - 1; // 배열의 길이 -1
+        int mid; // (statt+end) /2
+
+        //1.찾고자 하는 값이 Mid의 값보다 큰 경우 start +1.
+        //2.찾고자 하는 값이 Mid의 값보다 작은 경우 start -1.
+
+
+        while (start <= end) {
+            
+        }
+
+
+
+    }
+}
