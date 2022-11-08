87783ff46f7cfffebabb57828f7bfef6e5b29093
KimGunWoo9595 <medneter@naver.com>
2022-11-08 09:24:44 +0900

    Scanner 완료

--- src/main/java/lionalgorithm/algorithm/day22/Solution.java
+++ src/main/java/lionalgorithm/algorithm/day22/Solution.java
@@ -1,15 +1,13 @@
 package lionalgorithm.algorithm.day22;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Solution {
     //https://codeup.kr/problem.php?id=3001
     //단순 탐색
-
-
     public int soltution(int n, int[] arr,int want) {
         int answer = 0;
-       
         System.out.println(Arrays.toString(arr));
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == want) {
@@ -22,9 +20,16 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        int n = 10;
-        int[] arr = {2, 3, 7, 4, 9, 10, 233, 111, 23, 39};
-        int want =111;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int want = sc.nextInt();
+
+
+
 
         Solution s = new Solution();
         int soltution = s.soltution(n, arr, want);
