70d89a3652ebb78643f644600131371c880130c8
KimGunWoo9595 <medneter@naver.com>
2022-11-16 10:27:09 +0900

    퀵 sort pivot을 기준으로 교환하기 진행중

--- src/main/java/lionalgorithm/algorithm/day29/QuickSort.java
+++ src/main/java/lionalgorithm/algorithm/day29/QuickSort.java
@@ -2,7 +2,7 @@ package lionalgorithm.algorithm.day29;
 
 public class QuickSort {
 
-    void pivotSort(int[] arr, int left, int right) {
+    public void pivotSort(int[] arr, int left, int right) {
         int pivot = arr[arr.length/2];
         left = 0; // 맨 왼쪽 인덱스의 시작점
         right = arr.length -1; // 맨 오른쪽 인덱스의 시작점
@@ -11,11 +11,16 @@ public class QuickSort {
         if (left >= right) return;
 
         while (left < right) {
-            while (arr[left] < pivot) {left++;} // 왼쪽의 영역이 작다면 index만 증가시켜줘서 다음값 비교하게해주자
-            while (arr[right] > pivot ) {right--;}
+            while (arr[left] < pivot) {left++;} // 왼쪽의 영역이 피봇보다 작다면 index만 증가시켜줘서 다음값 비교하게해주자
+            while (arr[right] > pivot ) {right--;} // 오른쪽의 영역이 피봇보다 크다면 index만 감소시켜서 이전값을 배교하게해주자
+
+            //교환
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            //교환 후에는 왼쪽인덱스는 +1 오른쪽인덱스는 -1을 해줘야한다.
+            left++;
+            right--;
         }
-
-
-
     }
 }
