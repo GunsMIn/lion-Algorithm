1f4e8a14f2ba1e204a5d62879e598726904da095
KimGunWoo9595 <medneter@naver.com>
2022-11-30 09:50:42 +0900

    노드간의 거리 주석 계산

--- src/main/java/lionalgorithm/algorithm/day34/Tree.java
+++ src/main/java/lionalgorithm/algorithm/day34/Tree.java
@@ -2,6 +2,10 @@ package lionalgorithm.algorithm.day34;
 
 public class Tree {
 
+    /**부모가 같을때까지 부모를 찾으면서
+     * 한칸 올라갈때마다 +1을 하는 방식으로 거리를 구한다..
+     */
+
     public static int goUp(int a,int b) {
         if (a > b) {
             return goUp(a / 2, b) + 1; // a를 부모 쪽으로
@@ -11,6 +15,15 @@ public class Tree {
             return 0;
         }
     }
+    // 4 5 +1
+    // 2 5 +1
+    //2 2 0  -> 거리 : 2
+    /////////////
+    // 4,3 +1
+    // 2,3 +1
+    // 2,1 +1
+    // 1,1 -> 거리 3
+
 
     public static void main(String[] args) {
         int i = goUp(3, 4);
