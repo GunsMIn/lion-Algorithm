2c6f1b3978a85fd21e74da793671cdbcce3e7d10
KimGunWoo9595 <medneter@naver.com>
2022-12-02 09:35:22 +0900

    소수구하기

---
+++ src/main/java/lionalgorithm/algorithm/day36/Solution.java
@@ -0,0 +1,26 @@
+package lionalgorithm.algorithm.day36;
+
+import java.util.Scanner;
+
+public class Solution {
+
+
+    public static void findPrimeFactor(int n) {
+        for (int i = 2; i < Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                System.out.println(i);
+                findPrimeFactor(n / i);
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Solution.findPrimeFactor(n);
+    }
+
+
+
+}
