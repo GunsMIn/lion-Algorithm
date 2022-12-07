16f59eac176b08dc97d494a8fb6c77736f9445ce
KimGunWoo9595 <medneter@naver.com>
2022-12-07 10:27:47 +0900

    우선순위 큐 수정(프로그래머스 더 맵게)

--- src/main/java/lionalgorithm/algorithm/day38/Priority.java
+++ src/main/java/lionalgorithm/algorithm/day38/Priority.java
@@ -4,49 +4,37 @@ import java.util.PriorityQueue;
 
 public class Priority {
 
-    public int solution(int[] scoville, int k) {
+    public int solution(int[] scoville, int K) {
         int answer = 0; // count
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> a - b);
         //우선 순위 큐에 값 넣기
         for (int s : scoville) {
             pq.add(s);
         }
-        while (pq.peek() < k) {
+        if(pq.size()<=1) return 0;
+        //모든 음식의 스코빌 지수를 K 이상으로 만든다
+        while (pq.peek() < K) {
+            System.out.println("pq.peek() = " + pq.peek());
             int firstValue = pq.poll();
             int secondValue = pq.poll();
-            int mix = firstValue + secondValue;
+            System.out.println("firstValue = " + firstValue);
+            System.out.println("secondValue = " + secondValue);
+            int mix = firstValue + (secondValue*2);
+            pq.offer(mix);
             answer ++;
         }
-
+        System.out.println("pq = " + pq);
         return answer;
     }
 
 
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> a-b);
-        int[] arr = {1, 2, 3, 9, 10, 12};
+
+        int[] scoville = {1, 2, 3, 9, 10, 12};
         int k = 7;
-        int cnt = 0;
-        for (int i = 0; i <arr.length ; i++) {
-            pq.add(arr[i]);
-        }
 
-       /* int mix = pq.poll() + (pq.poll() * 2);
-        System.out.println("mix = " + mix);
-        pq.add(mix);*/
-
-        while (pq.peek() < k) {
-            int mix = pq.poll() + (pq.poll() * 2);
-            if (mix <= k) {
-                pq.add(mix);
-                cnt++;
-            }
-        }
-        System.out.println("pq = " + pq);
-        System.out.println("cnt = " + cnt);
-
-
-
+        int solution = new Priority().solution(scoville, k);
+        System.out.println("solution = " + solution);
 
 
       /*  pq.add(1);		pq.add(15);		pq.offer(10);
