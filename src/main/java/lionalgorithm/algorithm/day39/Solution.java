488579ff22e69aed68246c375efbfb06deb2a0f9
KimGunWoo9595 <medneter@naver.com>
2022-12-08 10:07:44 +0900

    피보나치 재귀 dp

--- src/main/java/lionalgorithm/algorithm/day39/Solution.java
+++ src/main/java/lionalgorithm/algorithm/day39/Solution.java
@@ -3,18 +3,23 @@ package lionalgorithm.algorithm.day39;
 public class Solution {
 
 
-    public static Long fib(long n, long[] memo) {
+    public static long fib(int n, long[] memo) {
 
-        if (n < 1) return 1;
-        if (memo[(int)n] == 0) {
-            memo[(int) n] = fib(n - 1, memo) + fib(n - 2, memo);
+        if(n == 0) throw new IllegalStateException("n은 1이상의 수");
+        if(n == 1 || n == 2) return 1;
+        //인덱스 1,2는 1 반환
+        if (memo[n] == 0) { // memo 배열에 초기값 0이 들어있다.
+            memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+            //memo[5] = fib(4) + fib(3)
         }
         return memo[n];
     }
 
 
     public static void main(String[] args) {
-
+        int n = 5;
+        var r = fib(n, new long[n + 1]);
+        System.out.println(r);
 
     }
 }
