f9360df30bcb68c233003e4798c2b40aed1bf36e
KimGunWoo9595 <medneter@naver.com>
2022-12-17 12:11:47 +0900

    알고리즘 List 완주하지 못한 선수

--- src/main/java/lionalgorithm/algorithm/day19/Solution5.java
+++ src/main/java/lionalgorithm/algorithm/day19/Solution5.java
@@ -1,4 +1,42 @@
 package lionalgorithm.algorithm.day19;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution5 {
+
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        //["119", "97674223", "1195524421"]
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : phone_book) {
+            map.put(s, 1);
+        }
+
+        for (int i = 0; i < phone_book.length; i++) {
+            for (int j = 1; j < phone_book[i].length(); j++) {
+                if (map.containsKey(phone_book[i].substring(0, j))) {
+                    return false;
+                }
+            }
+        }
+
+
+        return answer;
+    }
+
+    public boolean solution2(String[] phone_book) {
+        boolean answer = true;
+        //["119", "97674223", "1195524421"]
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
+            }
+        }
+
+        return answer;
+    }
 }
--- src/main/java/lionalgorithm/algorithm/day19/SolutionKakao.java
+++ src/main/java/lionalgorithm/algorithm/day19/SolutionKakao.java
@@ -1,4 +1,44 @@
 package lionalgorithm.algorithm.day19;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class SolutionKakao {
+    //실패율 클래스
+    class Fail{
+        int stage; // 1번 스테이지 2번스테이지 .....
+        double fail; //
+        public Fail(int stage, double fail) {
+            this.stage = stage;
+            this.fail = fail;
+        }
+    }
+
+
+
+    public int[] solution(int N, int[] stages) {
+      
+        //실패율 = 도달 했는데 클리어 못한 수 / 도달 한 전체 수
+        // int N = 전체 스테이지의 개수
+        // int[] stages 사용자가 현재 멈춰있는 스테이지의 번호가 담긴 배열[2, 1, 2, 6, 2, 4, 3, 3]
+        // stages.lentgth가 유저의 명수
+        int[] answer = new int[N];
+        List<Fail> fails = new ArrayList<>();
+        int total = stages.length; // 총 인원수
+
+        //users의 index는 stage의 번호이고 값은 stage에 도달한 사용자
+        int[] users = new int[N + 1];
+        //이제 각 stages를 순회를 할 것
+        for (int s : stages) {
+          /*  users[s]*/
+        }
+        
+        
+        
+        
+        
+        return answer;
+    }
 }
